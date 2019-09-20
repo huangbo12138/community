132dda6fd91c76bddc0e276706ef405ccdca3a00
huangbo12138 <490365453@qq.com>
2019-09-20 09:14:10 +0800

    init

---
+++ src/main/java/org/huboy1/huboy1/Huboy1Application.java
@@ -0,0 +1,13 @@
+package org.huboy1.huboy1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Huboy1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Huboy1Application.class, args);
+    }
+
+}
